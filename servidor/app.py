f2a3672514719c9dfb4a7614b6c92cded01f8d72
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-08-13 09:54:51 -0300

    utilizamos  postman para simular el metodo post

--- servidor/app.py
+++ servidor/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/sensor", methods = ["POST"])
+def sensor():
+    nombre = request.json  ['nombre']
+    valor = request.json  ['valor']
+    print(f"nombre :{nombre} , valor : {valor}") #se ve en la terminal
+    return "ok" #se ve en postman
+#lo corremos en postman ya que en el navegador no se puede utilizar el POST
