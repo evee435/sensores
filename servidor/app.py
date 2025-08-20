02742a2d3d0413402d89e7c698eacd2c9a075a9d
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-08-20 10:00:24 -0300

    agregue codigo de conexion a la base de datos

--- servidor/app.py
+++ servidor/app.py
@@ -1,6 +1,26 @@
-from flask import Flask, request
+from flask import Flask, g, request
+import sqlite3
 
 app = Flask(__name__)
+
+def dict_factory(cursor, row):
+ """Arma un diccionario con los valores de la fila."""
+ fields = [column[0] for column in cursor.description]
+ return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+  if 'db' not in g:
+     g.db = sqlite3.connect("valores.sqlite")
+     g.db.row_factory = dict_factory
+  return g.db
+
+
+def cerrarConexion(e=None):
+   db = g.pop('db', None)
+   if db is not None:
+       db.close()
+
+app.teardown_appcontext(cerrarConexion)
 
 @app.route("/")
 def hello_world():
@@ -8,8 +28,13 @@ def hello_world():
 
 @app.route("/api/sensor", methods = ["POST"])
 def sensor():
+    abrirConexion()
     nombre = request.json  ['nombre']
     valor = request.json  ['valor']
     print(f"nombre :{nombre} , valor : {valor}") #se ve en la terminal
+
+    cerrarConexion()
     return "ok" #se ve en postman
 #lo corremos en postman ya que en el navegador no se puede utilizar el POST
+
+#agregue codigo de conexion a la basa de datos
